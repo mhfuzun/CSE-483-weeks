708eb93c24951a4ad223c09e0b46bcafa045d40e
mhfuzun <mhfurkan.uzun@gmail.com>
2026-03-09 21:15:44 +0300

    week1-apple template

--- week1-opengl_instl/src/main.c
+++ week1-opengl_instl/src/main.c
@@ -1,4 +1,12 @@
-#include <GL/glut.h> // (or others, depending on the system in use)
+// (or others, depending on the system in use)
+#ifdef __APPLE__
+    #include <GLUT/glut.h>
+#else
+    #include <GL/glut.h>
+#endif
+
+int SCREEN_WIDTH    = 800;
+int SCREEN_HEIGHT   = 400;
 
 void init (void)
 {
@@ -9,21 +17,44 @@ void init (void)
 void lineSegment (void)
 {
     glClear (GL_COLOR_BUFFER_BIT); // Clear display window.
+    
     glColor3f (0.0, 0.0, 1.0); // Set line segment color to blue.
     glBegin (GL_LINES);
-    glVertex2i (180, 15); // Specify line-segment geometry.
-    glVertex2i (10, 145);
+        glVertex2i (180, 15); // Specify line-segment geometry.
+        glVertex2i (10, 145);
     glEnd ( );
+
+    // ground
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_QUADS);
+        glVertex2i(-SCREEN_WIDTH/2, -SCREEN_HEIGHT/2); // Sol Alt
+        glVertex2i(+SCREEN_WIDTH/2, -SCREEN_HEIGHT/2); // Sağ Alt
+        glVertex2i(-SCREEN_WIDTH/2, +(int)(SCREEN_HEIGHT*.5f)/2); // Sağ Üst
+        glVertex2i(+SCREEN_WIDTH/2, +(int)(SCREEN_HEIGHT*.5f)/2); // Sol Üst
+    glEnd();
+
+    // sky
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glBegin(GL_QUADS);
+        glVertex2i(-SCREEN_WIDTH/2, +(int)(SCREEN_HEIGHT*.5f)/2); // Sol Alt
+        glVertex2i(+SCREEN_WIDTH/2, +(int)(SCREEN_HEIGHT*.5f)/2); // Sağ Alt
+        glVertex2i(-SCREEN_WIDTH/2, +SCREEN_HEIGHT/2); // Sağ Üst
+        glVertex2i(+SCREEN_WIDTH/2, +SCREEN_HEIGHT/2); // Sol Üst
+    glEnd();
+    
     glFlush ( ); // Process all OpenGL routines as quickly as possible.
 }
-void main (int argc, char** argv)
+
+int main (int argc, char** argv)
 {
     glutInit (&argc, argv); // Initialize GLUT.
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB); // Set display mode.
     glutInitWindowPosition (50, 100); // Set top-left display-window position.
-    glutInitWindowSize (400, 300); // Set display-window width and height.
-    glutCreateWindow ("An Example OpenGL Program"); // Create display window.
+    glutInitWindowSize (SCREEN_WIDTH, SCREEN_HEIGHT); // Set display-window width and height.
+    glutCreateWindow ("HW 1 - Muhammet Furkan UZUN"); // Create display window.
     init ( ); // Execute initialization procedure.
     glutDisplayFunc (lineSegment); // Send graphics to display window.
     glutMainLoop ( ); // Display everything and wait.
+
+    return 0;
 }
